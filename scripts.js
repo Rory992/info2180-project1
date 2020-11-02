37c9486d3c6cefa68b808e58949f108fef905b71
Rory <youngrory992@gmail.com>
2020-11-01 23:47:34 -0500

    Updated Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-/* Add your JavaScript to this file */
+window.onload=function(){
+	let btn=document.getElementsByTagName("button")[0];
+	let message=document.getElementsByClassName("message")[0];
+	let email=document.getElementsById("email");
+	
+	btn.addEventListener("click", function(x){
+		x.preventDefault();
+		var email=emails.value;
+		
+		if (email.indexOf('@') <=0)
+		{
+			message.textContent="Please enter a valid email address";
+		}
+		else if (email.charAt(email.length-4)!=',')
+		{
+			message.textContent="Please enter a valid email address";
+		}
+		else {
+			message.textContent='Thank you! Your email address ${email} has been added to our mailing list!';
+			email.value = " ";
+		}
+    }
+    
+}
